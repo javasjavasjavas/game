4159c187db0c2b0f46ab0e8555ab00a531469f7d
javasjavasjavas <javdamico@gmail.com>
2026-03-11 18:12:48 -0300

    Init base project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
